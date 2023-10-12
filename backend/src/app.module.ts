6b5fd8fe26a8f06a80b87b3d3955708214643799
Amaroke <hugo.amaroke@gmail.com>
2023-10-12 10:50:20 +0200

    .env, db and docker configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
+
+config();
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
