8d91c0ea875c546eac965183c533286d4784e925
Amaroke <hugo.amaroke@gmail.com>
2023-10-20 19:16:24 +0200

    front left/right section modification

--- frontend/src/app/search-section/search-section.component.ts
+++ frontend/src/app/search-section/search-section.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: []
 })
 export class SearchSectionComponent {
+  activeTab = 'question'; // Initial tab is 'question'
+
+  showSearch(tab: string) {
+    this.activeTab = tab;
+  }
 
 }
